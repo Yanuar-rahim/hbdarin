38ae4ed71e98f4083bc0254c1b93c76f4f3078ec
Yanuar Rahim <yanuarrahim519@gmail.com>
2026-03-06 11:15:47 +0800

    update

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 const konten = [
 
     {
-        foto: "https://i.imghippo.com/files/FCio9100FEs.jpg",
+        foto: "assets/1.jpg",
         ucapan: "hallo sayangkuu, selamat ulang tahun. tidak terasa hari ini adalah hari yang sangat spesial, karena di hari inilah seseorang yang begitu berarti dalam hidupku dilahirkan.🤍✨🎉"
     },
 
     {
-        foto: "https://i.imghippo.com/files/pTEm2996lLA.jpg",
+        foto: "assets/2.jpg",
         ucapan: "sa bersyukur bisa kenal kau, bisa dekat dengan kau, dan bisa jadi bagian dari perjalanan hidupmu sampai saat ini. semoga di usiamu yang baru ini, kamu selalu diberikan kebahagiaan, kesehatan, dan segala hal baik yang kamu impikan dapat tercapai. aminnn.🤲🏻🫶🏻✨"
     },
 
     {
-        foto: "https://i.imghippo.com/files/CbH9153fHY.png",
+        foto: "assets/3.png",
         ucapan: "di hari spesialmu ini, saya juga mau minta maaf atas segala kesalahanku. maaf kalau ada kata-kata atau sikapku yang pernah menyakiti hatimu atau mengecewakanmu. sa sadar sa belum sempurna, tapi sa akan terus berusaha menjadi lebih baik untukmu dan untuk hubungan kita.🥺🫶🏻🤍"
     },
 
     {
-        foto: "https://i.imghippo.com/files/rBK4926wsc.jpg",
+        foto: "assets/4.jpg",
         ucapan: "sa berharap semoga di setiap langkah yang kamu jalani ke depan, kamu selalu dikelilingi oleh hal-hal baik. semoga semua mimpi dan harapanmu dapat tercapai. kamu adalah perempuan yang kuat dan hebat, serta pantas mendapatkan kebahagiaan yang besar dalam hidupmu. aminnn🤲🏻🤍✨"
     },
 
     {
-        foto: "https://i.imghippo.com/files/IDr3928io.jpg",
+        foto: "assets/5.jpg",
         ucapan: "sekali lagi selamat ulang tahun, cintaku. semoga hari ini dipenuhi kebahagiaan dan doa-doa baik dari orang-orang yang menyayangimu. terima kasih sudah hadir dalam hidup saya dan menjadi bagian penting dalam setiap cerita saya. semoga kita bisa terus bersama dan menciptakan lebih banyak kenangan indah di masa depan. love you 🫶🏻🤍✨"
     }
 
